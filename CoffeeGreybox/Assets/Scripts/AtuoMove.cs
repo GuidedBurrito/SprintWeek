1938ab367d19c7b9ebf63dbb1d63aef229d6b1f8
JesiMcMulkin <jesimc@yahoo.ca>
2016-03-07 17:16:30 -0500

    Unity Graybox

---
+++ CoffeeGreybox/Assets/Scripts/AtuoMove.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AtuoMove : MonoBehaviour {
+
+    public GameObject player;
+
+    float playerSpeed;
+    public float defaultSpeed;
+    public float upSpeed;
+    public float downSpeed;
+
+    public float laneWidth;
+
+
+    float currentAxis;
+
+    public float timeScore;
+
+	// Use this for initialization
+	void Start () {
+        playerSpeed = defaultSpeed;
+	 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Automatic player movement
+        this.transform.position += Vector3.right * Time.deltaTime * playerSpeed;
+
+        //Player input
+        if (Input.GetAxis("Vertical") > currentAxis && (Input.GetAxis("Vertical") > 0))
+        {
+            //Move player up a lane, unless they're in the top lane
+            player.transform.position += Vector3.forward * laneWidth;
+        }
+
+        if (Input.GetAxis("Vertical") < currentAxis && (Input.GetAxis("Vertical") < 0))
+        {
+            //Move player down a lane, unless they're in the bottom lane
+            player.transform.position -= Vector3.forward * laneWidth;
+        }
+        if (Input.GetAxis("Fire1") > 0)
+        {
+            playerSpeed = upSpeed;
+        }
+        else if (Input.GetAxis("Fire1") < 0)
+        {
+            playerSpeed = downSpeed;
+        }
+        else
+        {
+            playerSpeed = defaultSpeed;
+        }
+
+        currentAxis = Input.GetAxis("Vertical");
+
+        //Display level time
+        timeScore = Time.time;
+
+    }
+}
---
+++ CoffeeGreybox/Assets/Scripts/Health.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+    public int maxHealth;
+    public int health;
+
+    public GameObject[] healthDisplay;
+
+	// Use this for initialization
+	void Start () {
+
+        health = maxHealth;
+        HealthChange();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            health -= 1;
+            HealthChange();
+        }
+        else if (other.gameObject.tag == "Coffee")
+        {
+            health += 1;
+            HealthChange();
+        }
+    }
+
+    void HealthChange()
+    {
+        for (int i = 0; i < health; i++)
+        {
+            healthDisplay[i].SetActive(true);
+        }
+        for (int i = health; i < healthDisplay.Length; i++)
+        {
+            healthDisplay[i].SetActive(false);
+        }
+    }
+}
