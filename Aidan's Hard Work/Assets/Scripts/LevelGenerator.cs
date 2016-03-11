46690cccbd2a798a23c21ffa337bee47e73b36e6
GuidedBurrito <alexvalickis@hotmail.com>
2016-03-10 21:00:23 -0500

    Map and Enemy Spawner
    If you need help with this lemme know

---
+++ Aidan's Hard Work/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public GameObject[] tileSet;
+
+    SpriteRenderer rend;
+    float tileWidth;
+
+    GameObject newTile;
+
+    float nextXValue;
+
+    void Start()
+    {
+        tileWidth = tileSet[0].GetComponent<SpriteRenderer>().bounds.size.x;
+        nextXValue = this.transform.position.x;
+    }
+
+    void Update()
+    {
+        if (this.transform.position.x > nextXValue)
+        {
+            SpawnNewFloorTile();
+        }
+    }
+
+
+    void SpawnNewFloorTile()
+    {
+
+        int tileToSpawn = Random.Range(0, tileSet.Length);
+        newTile = (GameObject)Instantiate(tileSet[tileToSpawn], new Vector3(nextXValue, this.transform.position.y, 0), Quaternion.identity);
+        float tileStartX = newTile.transform.position.x;
+        nextXValue = tileStartX + tileWidth;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //    if (spawner.transform.position.x > previousTile.transform.position.x)
+    //        {
+    //            SpawnNewFloorTile();
+    //}
+
+
+
+    //void SpawnNewFloorTile()
+    //{
+
+    //    int tileToSpawn = Random.Range(0, tileSet.Length);
+    //    newTile = (GameObject)Instantiate(tileSet[tileToSpawn], new Vector3(spawner.transform.position.x + nextXValue, spawner.transform.position.y, 0), Quaternion.identity);
+    //    print("spawned");
+    //    float tileStartX = newTile.transform.position.x;
+    //    //print(tileStartX);
+
+    //    float tileWidth = newTile.GetComponent<SpriteRenderer>().bounds.size.x;
+    //    //print(tileWidth);
+
+    //    nextXValue = tileStartX + tileWidth;
+    //    // print("next x" + nextXValue);
+    //    previousTile = newTile;
+    //    Spawn(previousTile);
+    //}
+
+    //void Spawn(GameObject previousTile)
+    //{
+
+
+    //}
+
+}
---
+++ Aidan's Hard Work/Assets/Scripts/Obstacle Controllers/ObstacleBaseScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RegularPerson : MonoBehaviour {
+    public float speed;
+    public float chanceToStop;
+    public float chanceToSpawn;
+    public int damageValue;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+     
+}
